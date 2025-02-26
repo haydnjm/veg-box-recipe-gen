be16ef196ecdd1882e406738247197ca48ba49f9
Haydn <haydn.morris@ivy-solutions.de>
2025-02-26 16:19:55 +0100

    update env, update deps

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,10 @@
-import dotenv from "dotenv";
-dotenv.config();
+import "dotenv/config";
+
 import { createRecipes } from "./recipes.js";
 import { sendRecipeEmail } from "./emails/index.js";
-// Main cloud function
+
 export const sendRecipeEmails = async (): Promise<void> => {
-  console.log(
-    "Sending recipe emails... test: ",
-    process.env.TEST_RUN ? "true" : "false"
-  );
+  console.log("Sending recipe emails... test: ", process.env.TEST_RUN);
   try {
     const recipes = await createRecipes();
 
